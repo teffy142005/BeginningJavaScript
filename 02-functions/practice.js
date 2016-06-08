c71f2ce8898764ffaca5155240c2424f13f68345
teffy142005 <stephanie.carvajal-garcia@hpe.com>
2016-06-08 15:53:20 -0600

    Update practice.js

--- 02-functions/practice.js
+++ 02-functions/practice.js
@@ -1,68 +1,43 @@
-// (example) Write a function that accepts two number arguments and adds them
-// together.
-var add = function () {
+
+
+var add = function (x,y) {
+	var mas= ""
+	return x + mas + y;
 };
 
 
-// Create a function that takes in a cost and a quantity and outputs the total
-// pre-tax cost for that quantity of items at the given price. For example
-//
-//     var preTaxTotal = totalCost(5, 5.99); // 5 items at 5.99
-//     //=> 29.950000000000003
-var totalCost = function () {
+
+
+var totalCost = function (x,y) {
+
+   return x * y;
 };
 
 
-// Write a function that takes a rank and a suit as input, and returns a string
-// representation of a card. For example, it should behave as follows
-//
-//     cardString("ace", "spades");
-//     //=> ace of spades
-//
-//     cardString("queen", "hearts");
-//     //=> queen of hearts
-var cardString = function () {
+
+var cardString = function (rank,suit) {
+
+ var of = "of"; 
+return rank + of + suit;
+
 };
 
 
-// Write a function called `openTag` that accepts a tag name and returns an
-// opening HTML tag with that tag name. For example,
-//
-//     openTag("p");
-//     //=> <p>
-//
-//     openTag("div");
-//     //=> <div>
-var openTag = function () {
+
+
+var opentag = function(tag){
+
+var open= "<"+tag+">";
+return open;
+
+}
+
+var htmlElement= function (tag,sentense) {
+
+var concat= "<"+ tag + ">";
+var concact2="<"+ tag + "/>"
+var msj= concat + sentense + concact2;
+
+return msj;
+
 };
-
-
-// Similarly, write a function called `closingTag` that returns the closing HTML tag
-// with that name.
-//
-//     closeTag("p");
-//     //=> </p>
-//
-//     closeTag("div");
-//     //=> </div>
-var closeTag = function () {
-};
-
-
-// Write a function called `toTagString` that accepts two inputs: a tag name,
-// and the string content, and returns the content tagged with the specified
-// string.  For example:
-//
-//     toTagString("li", "list item 1");
-//     //=> <li>list item 1</li>
-//
-//     // the 'em' tag is for 'emphasis'
-//     toTagString("em", "this is important stuff");
-//     //=> <em>this is important stuff</em>
-//
-// Although I won't be testing for this, your code should re-use the functions that
-// you created in the previous section.
-var toTagString = function () {
-};
-
-
